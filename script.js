e4fbaa98b3d202ae656daecf45b6fb39f65c7c67
Vladyslav <vados1789@gmail.com>
2021-09-04 02:35:02 +0200

    Game ready

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 'use strict';
 
-let guessNumber = Number(document.querySelector('.number').value = Math.trunc(Math.random()*20));
+let guessNumber =  Math.trunc(Math.random()*20) +1;
 let score = Number(document.querySelector('.score').value = 20);
-let highScore = Number(document.querySelector('.highscore').value = 0);
+let highScore = 0
+document.querySelector('.number').textContent = guessNumber;
 
 document.querySelector('.check').addEventListener('click', function (){
     const guess = Number(document.querySelector('.guess').value);
@@ -10,22 +11,60 @@ document.querySelector('.check').addEventListener('click', function (){
         console.log('You win');
         document.querySelector('.number').textContent = String(guessNumber);
         document.querySelector('.message').textContent = "You win";
+        document.querySelector('body').style.backgroundColor = 'green';
+        if (Number(document.querySelector('.highscore').textContent) < score){
+            document.querySelector('.highscore').textContent = score;
+        }
     } else {
         score -- ;
-        highScore ++;
-        if (Number(document.querySelector('.guess').value) > guessNumber){
-            document.querySelector('.message').textContent = "Too big";
+        if (score > 0){
+            if (guess > guessNumber){
+                document.querySelector('.message').textContent = "Too big";
+            } else {
+                document.querySelector('.message').textContent = "Too low";
+            }
+            document.querySelector('.score').textContent = String(score);
+            console.log('Try again', guessNumber);
         } else {
-            document.querySelector('.message').textContent = "Too low";
+            document.querySelector('.message').textContent = 'You lose the game';
+            document.querySelector('.score').textContent = '0';
+            document.querySelector('body').style.backgroundColor = 'red';
         }
-        document.querySelector('.score').textContent = String(score);
-        document.querySelector('.highscore').textContent = String(highScore);
-        console.log('Try again', guessNumber);
+
+    }
+});
+
+document.querySelector('.check').addEventListener('Enter', function (){
+    const guess = Number(document.querySelector('.guess').value);
+    if (guessNumber === guess){
+        console.log('You win');
+        document.querySelector('.number').textContent = String(guessNumber);
+        document.querySelector('.message').textContent = "You win";
+        document.querySelector('body').style.backgroundColor = 'green';
+        if (Number(document.querySelector('.highscore').textContent) < score){
+            document.querySelector('.highscore').textContent = score;
+        }
+    } else {
+        score -- ;
+        if (score > 0){
+            if (guess > guessNumber){
+                document.querySelector('.message').textContent = "Too big";
+            } else {
+                document.querySelector('.message').textContent = "Too low";
+            }
+            document.querySelector('.score').textContent = String(score);
+            console.log('Try again', guessNumber);
+        } else {
+            document.querySelector('.message').textContent = 'You lose the game';
+            document.querySelector('.score').textContent = '0';
+            document.querySelector('body').style.backgroundColor = 'red';
+        }
+
     }
 });
 document.querySelector('.again').addEventListener('click', function (){
-    guessNumber = Number(document.querySelector('.number').value = Math.trunc(Math.random()*20));
+    guessNumber = Math.trunc(Math.random()*20);
     score = Number(document.querySelector('.score').value = 20);
-    highScore = Number(document.querySelector('.highscore').value = 0);
     document.querySelector('.guess').value = null;
+    document.querySelector('body').style.backgroundColor = '#222';
 });
